6426f1d020a432229923b4351fe63058ba99ca03
ZhiweiYan-96 <zhiwei.yan@intel.com>
2025-02-27 04:34:14 +0000

    [Intel GPU] Use independent oneDNN
    ghstack-source-id: 5f8776d3d0db9e55e6e5c3e87b9333550ccacefb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/147926

--- cmake/Modules/FindMKLDNN.cmake
+++ cmake/Modules/FindMKLDNN.cmake
@@ -48,7 +48,8 @@ IF(NOT MKLDNN_FOUND)
       set(DNNL_CXX_FLAGS "")
     endif()
     ExternalProject_Add(xpu_mkldnn_proj
-      SOURCE_DIR ${MKLDNN_ROOT}
+      GIT_REPOSITORY https://github.com/oneapi-src/oneDNN
+      GIT_TAG v3.7
       PREFIX ${XPU_MKLDNN_DIR_PREFIX}
       BUILD_IN_SOURCE 0
       CMAKE_ARGS  -DCMAKE_C_COMPILER=icx
@@ -67,10 +68,12 @@ IF(NOT MKLDNN_FOUND)
       INSTALL_COMMAND ""
     )
 
-    ExternalProject_Get_Property(xpu_mkldnn_proj BINARY_DIR)
+    ExternalProject_Get_Property(xpu_mkldnn_proj BINARY_DIR SOURCE_DIR)
+
     set(__XPU_MKLDNN_BUILD_DIR ${BINARY_DIR})
+    set(__XPU_MKLDNN_SRC_DIR ${SOURCE_DIR})
     set(XPU_MKLDNN_LIBRARIES ${__XPU_MKLDNN_BUILD_DIR}/src/${DNNL_LIB_NAME})
-    set(XPU_MKLDNN_INCLUDE ${__XPU_MKLDNN_BUILD_DIR}/include)
+    set(XPU_MKLDNN_INCLUDE ${__XPU_MKLDNN_BUILD_DIR}/include ${__XPU_MKLDNN_SRC_DIR}/include)
     # This target would be further linked to libtorch_xpu.so.
     # The libtorch_xpu.so would contain Conv&GEMM operators that depend on
     # oneDNN primitive implementations inside libdnnl.a.
